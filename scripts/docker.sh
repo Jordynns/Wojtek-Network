9578cf49af4d837a5ee88addd48fb3d8aea8a780
Jordyn <133882087+Jordynns@users.noreply.github.com>
2025-11-06 12:52:05 +0000

    Update docker.sh

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -2,14 +2,14 @@
 
 sudo apt update && sudo apt upgrade -y
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io -y
 sudo systemctl enable docker
 sudo systemctl start docker
-docker volume create portainer_data
-docker run -d -p 8000:8000 -p 9443:9443 \
+sudo docker volume create portainer_data
+sudo docker run -d -p 8000:8000 -p 9443:9443 \
     --name=portainer \
     --restart=always \
     -v /var/run/docker.sock:/var/run/docker.sock \
