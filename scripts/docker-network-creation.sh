24dc52281e83246fca0eeecab7aedd5c84355e16
Jordyn <133882087+Jordynns@users.noreply.github.com>
2026-02-03 13:20:45 +0000

    Update docker-network-creation.sh

--- scripts/docker-network-creation.sh
+++ scripts/docker-network-creation.sh
@@ -6,6 +6,12 @@ sudo docker network create -d ipvlan \
   -o parent=eth0 \
   ip_vlan
 
+sudo ip link add ipvl0 link eth0 type ipvlan mode l2
+sudo ip addr add 192.168.1.10/24 dev ipvl0
+sudo ip link set ipvl0 up
+sudo ip route add 192.168.1.0/24 dev ipvl0
+
+
 echo "=== Docker Network ==="
 echo "✅ Created ip_vlan"
 echo "-   Subnet: 192.168.1.0/24"
