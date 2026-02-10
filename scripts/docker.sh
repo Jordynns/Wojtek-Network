da74045ba0c4a62140deb7f46afcbd9edbb51dfa
Jordyn <133882087+Jordynns@users.noreply.github.com>
2026-02-10 13:54:41 +0000

    Update docker.sh

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -5,7 +5,7 @@ sudo apt install apt-transport-https ca-certificates curl software-properties-co
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
-sudo apt install docker-ce=5:28.5.2-1~ubuntu.24.04~noble docker-ce-cli=5:28.5.2-1~ubuntu.24.04~noble containerd.io -y
+sudo apt install docker-ce=5:28.5.2-1~ubuntu.24.04~noble docker-ce-cli=5:28.5.2-1~ubuntu.24.04~noble docker-compose containerd.io -y
 sudo systemctl enable docker
 sudo systemctl start docker
 sudo docker volume create portainer_data
