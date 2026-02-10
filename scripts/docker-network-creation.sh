c7b482611513f2ba2fa680e0c5d55a5ab68c782a
Jordyn <133882087+Jordynns@users.noreply.github.com>
2026-02-10 14:27:57 +0000

    Update docker-network-creation.sh

--- scripts/docker-network-creation.sh
+++ scripts/docker-network-creation.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-sudo docker network create -d caddy \
+sudo docker network create -d ip_vlan \
   --subnet=192.168.10.0/24 \
   --gateway=192.168.10.1 \
   -o parent=eth0 \
-  caddy
+  ip_vlan
 
 echo "=== Docker Network ==="
 echo " ✅ Created caddy Network"
 echo "-   Subnet: 192.168.10.0/24"
 echo "-   Gateway: 192.168.10.1"
 echo "-   Parent NIC ID: eth0"
-echo "-   Network Name: caddy"
+echo "-   Network Name: ip_vlan"
 echo "======================="
 
 rm -- "$0"
