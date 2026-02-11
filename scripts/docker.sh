7345ea98f0ee9c6d36fa98ea4597f3087bc0e2e8
Jordyn <133882087+Jordynns@users.noreply.github.com>
2026-02-11 03:25:32 +0000

    Enhance docker.sh with Docker and Portainer setup
    Added installation steps for Docker, Portainer, and Jellyfin, including network setup and configuration.

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
+set -e
 
+# Define Variables
+NAS_PATH="/srv/storage"
+DASHY_CFG="https://raw.githubusercontent.com/Jordynns/Wojtek-Network/refs/heads/main/scripts/config/dashy/conf.yml"
+
+# Upgrade System
 sudo apt update && sudo apt upgrade -y
+
+# Install Certs for Docker
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
+
+# Install & Enable Docker
 sudo apt install docker-ce=5:28.5.2-1~ubuntu.24.04~noble docker-ce-cli=5:28.5.2-1~ubuntu.24.04~noble containerd.io -y
 sudo systemctl enable docker
 sudo systemctl start docker
+
+# Create Portainer Container
 sudo docker volume create portainer_data
 sudo docker run -d -p 9000:9000 \
     --name=portainer \
@@ -16,13 +28,41 @@ sudo docker run -d -p 9000:9000 \
     -v portainer_data:/data \
     portainer/portainer-ce:latest
 
+# Create Docker Network for Containers
+sudo docker network create -d ipvlan \
+  --subnet=192.168.10.0/24 \
+  --gateway=192.168.10.1 \
+  -o parent=eth0 \
+  ip_vlan
+
+
+# Creat Jellyfin Directories on the NAS
+sudo mkdir -p \
+  "$NAS_PATH/jellyfin/cache" \
+  "$NAS_PATH/jellyfin/config" \
+  "$NAS_PATH/jellyfin/media"/{Movies,TV,Anime,YouTube}
+
+# Set Permissions (ROOT)
+sudo chown -R 1000:1000 "$NAS_PATH/jellyfin"
+sudo chmod -R 775 "$NAS_PATH/jellyfin/media"
+sudo chmod -R 775 "$NAS_PATH/jellyfin/config"
+sudo chmod -R 775 "$NAS_PATH/jellyfin/cache"
+
+# Dashy
+sudo mkdir -p /home/dashy
+sudo curl -fsSL "$DASHY_CFG" -o /home/dashy/conf.yml
+
 
 echo "=== Docker & Portainer Install ==="
-
 echo "✅ Installed Successfully"
-
-echo "Container: Portainer"
-echo "IP: $(ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'):9000"
 echo "=================================="
+
+echo "=== Docker Network ==="
+echo "✅  Created ip_vlan"
+echo "-   Subnet: 192.168.10.0/24"
+echo "-   Gateway: 192.168.10.1"
+echo "-   Parent NIC ID: eth0"
+echo "-   Network Name: ipvlan"
+echo "======================="
 
 rm -- "$0"
