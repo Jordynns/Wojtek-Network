0ff92b1242f73e4d264efa013a613c7d399cc76f
Jordyn <133882087+Jordynns@users.noreply.github.com>
2026-02-11 01:48:35 +0000

    Update file-path-gen.sh

--- scripts/file-path-gen.sh
+++ scripts/file-path-gen.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 set -e
 
+# Define Variables
 NAS_PATH="/srv/storage"
 DASHY_CFG="https://raw.githubusercontent.com/Jordynns/Wojtek-Network/refs/heads/main/scripts/config/dashy/conf.yml"
 
+# Creat Jellyfin Directories on the NAS
 sudo mkdir -p \
   "$NAS_PATH/jellyfin/cache" \
   "$NAS_PATH/jellyfin/config" \
   "$NAS_PATH/jellyfin/media"/{Movies,TV,Anime,YouTube}
 
+# Set Permissions (ROOT)
+sudo chown -R 1000:1000 "$NAS_PATH/jellyfin"
+sudo chmod -R 775 "$NAS_PATH/jellyfin/media"
+sudo chmod -R 775 "$NAS_PATH/jellyfin/config"
+sudo chmod -R 775 "$NAS_PATH/jellyfin/cache"
+
+# Dashy
 sudo mkdir -p /home/dashy
 sudo curl -fsSL "$DASHY_CFG" -o /home/dashy/conf.yml
