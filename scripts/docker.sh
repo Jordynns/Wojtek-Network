b00bc25ed9d94a10112526de0422eda2fd481535
Jordyn <133882087+Jordynns@users.noreply.github.com>
2026-02-12 09:33:31 +0000

    Update docker.sh

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -65,6 +65,11 @@ sudo mkdir -p \
     "home/grafana/data"
 sudo curl -fsSL "$PROMETHEUS_CFG" -o /home/prometheus/prometheus.yml
 
+# RetroArch Directories
+sudo mkdir -p \
+    "/home/retroarch/config" \
+    "/home/retroarch/roms"
+
 
 echo "=== Docker & Portainer Install ==="
 echo "✅ Installed Successfully"
